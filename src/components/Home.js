5a258ae53b6fb91295d0c14e481a173679717380
Arun Singh <arunsinghsnd@gmail.com>
2021-05-28 15:59:56 +0530

    added the .evn file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,29 @@
 import React from "react";
-//import { connect } from "react-redux";
+import { connect } from "react-redux";
 import { Button } from "react-bootstrap";
 import Header from "./Header";
 
-const Home = () => {
+const Home = props => {
+  const {
+    REACT_APP_CLIENT_ID,
+    REACT_APP_AUTHORIZE_URL,
+    REACT_APP_REDIRECT_URL,
+  } = process.env;
+
+  const handleLogin = () => {
+    window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
+    console.log("Clicked me");
+  };
+
   return (
     <div className="login">
       <Header />
-      <Button variant="info" type="submit">
+      <Button variant="info" type="submit" onClick={handleLogin}>
         Login to app
       </Button>
     </div>
   );
 };
 
-//export default connect()(Home);
-export default Home;
+export default connect()(Home);
+//export default Home;
